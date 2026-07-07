e0a9e96aec08bc26016b7ea10732b7fdcc9243e6
Mohamed Ismail <mohammedismail4565@gmail.com>
2026-07-07 22:18:33 +0530

    Update alert.js
    added railway url

--- FrontPart/Js/alert.js
+++ FrontPart/Js/alert.js
@@ -16,7 +16,7 @@ async function createAlert(){
 
     try{
 
-        const response=await fetch("http://localhost:8080/api/alerts",{
+        const response=await fetch("https://sincere-perfection-production-b375.up.railway.app/api/alerts",{
 
             method:"POST",
 
