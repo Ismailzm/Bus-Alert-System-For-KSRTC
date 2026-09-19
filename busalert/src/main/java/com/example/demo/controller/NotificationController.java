d1d9b16a72ac934feca7424564b265738fb74d6e
Mohamed Ismail <mohammedismail4565@gmail.com>
2026-09-19 21:28:12 +0530

    Update NotificationController.java

--- busalert/src/main/java/com/example/demo/controller/NotificationController.java
+++ busalert/src/main/java/com/example/demo/controller/NotificationController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +19,25 @@ public class NotificationController {
     }
 
     @GetMapping("/user/{userId}")
-    public List<Notification> getNotifications(@PathVariable Long userId) {
+    public List<Notification> getNotifications(
+            @PathVariable Long userId) {
 
         return notificationService.getNotifications(userId);
-
     }
 
+    // Delete one notification
+    @DeleteMapping("/{id}")
+    public void deleteNotification(
+            @PathVariable Long id) {
+
+        notificationService.deleteNotification(id);
+    }
+
+    // Delete all notifications of a user
+    @DeleteMapping("/user/{userId}")
+    public void deleteAllNotifications(
+            @PathVariable Long userId) {
+
+        notificationService.deleteAllNotifications(userId);
+    }
 }
